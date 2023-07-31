54dc01482e1f8e1ec91f773c0f1530413041549a
HP <HP@DESKTOP-1STKR3G>
2023-07-31 09:47:09 +0530

    fourth commit

---
+++ src/com/cg/dayone/Identifier.java
@@ -0,0 +1,14 @@
+package com.cg.dayone;
+
+public class Identifier {
+	 
+
+		public static void main(String[] args) {
+			String name="vana";
+			int age=21;
+			int id=30;
+			System.out.println(name+" "+ age +" "+ id+" ");
+
+		}
+	}
+
